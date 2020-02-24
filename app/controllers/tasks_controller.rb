4891e172fc6d7b77f394313f5940ff3b198a915c
Matt Parr <mattt.parr@gmail.com>
2020-02-24 16:54:05 +0000

    added 'strike through' to completed items

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,15 +3,15 @@ class TasksController < ApplicationController
   def index
     render json: Task.order(:id)
   end
-
+  
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+  
   def update
     task = Task.find(params[:id])
     task.update_attributes(task_params)
-    render json: task
-  end
-
-  def create
-    task = Task.create(task_params)
     render json: task
   end
 
